4de75839e7db89b20aff98d8107ea3c087e90223
nkurunziza-saddy <saadnkurunziza@email.com>
2025-10-03 20:57:06 +0200

    fixed default theme

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -11,7 +11,7 @@ export function ThemeProvider({
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
+      defaultTheme="supabase"
       themes={THEME_OPTIONS}
       enableSystem
       disableTransitionOnChange
